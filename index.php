e14c90f54be2db54534daf5098780b62fda52149
thath <thath@pdx.edu>
2011-06-08 19:45:03 -0700

    point of entry file

---
+++ index.php
@@ -0,0 +1,81 @@
+<?php
+
+// import phpCAS lib
+include_once('CAS.php');
+
+//phpCAS::setDebug();
+
+// initialize phpCAS
+phpCAS::client(CAS_VERSION_2_0,'sso.pdx.edu/cas',443,'');
+
+// no SSL validation for the CAS server
+phpCAS::setNoCasServerValidation();
+
+// force CAS authentication
+phpCAS::forceAuthentication();
+
+//Get users in to an array from text file
+
+$userfile = fopen("qtUsers.txt","r");
+$tempString = fread($userfile, filesize('qtUsers.txt'));
+$userArray = explode(',',$tempString);
+
+//set username variable
+$user = phpCAS::getUser();
+
+// Check to see if users are in the Array
+if (!in_array($user, $userArray)){
+	header('Location: https://oit.pdx.edu/qt/notregistered.html');
+}
+?>
+<!DOCTYPE XHTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<title>
+Quick ticket V2
+</title>
+<link href="css/quickticket.css" rel="stylesheet" type="text/css" />
+<link href="jquery-ui/themes/south-street/jquery-ui-1.8.13.custom.css" rel="stylesheet" type="text/css" />
+<link rel="icon" href="favicon.ico" type="image/x-icon"> 
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<script language="javascript" type="text/javascript" src="js/quickticket.js"></script>
+<script language="javascript" type="text/javascript" src="jquery-ui/jquery-1.5.1.js"></script>
+<script language="javascript" type="text/javascript" src="jquery-ui/ui/jquery-ui-1.8.13.custom.js"></script>
+<script>
+$(document).ready(function(){
+	$('#odin').focus();
+	$('#odin').change(function() {
+		sendOdin();
+	});
+	$('#quickSubject').change(function() {
+		setFormSubject();
+		showWRKBNCHforms();
+		setQueue();
+	});
+	$('.user-data button').click(function() {
+		alert($('body div #creator').val());
+	});
+});
+	$(function() {
+		$("#tabs").tabs();
+	});
+
+</script>
+</head>
+<body> 
+<div id="creator" style="display: none"><?php echo phpCAS::getUser();?></div> 
+<h1>Quick Ticket</h1><img id="psuLogo" src="images/portland_state_logo.gif"/>
+<div id="tabs">
+	<ul>
+		<li><a href="#tabs-1">Regular Ticket</a></li>
+		<li><a href="#tabs-2">Workbench Ticket</a></li>
+	</ul>
+<div id="tabs-1">
+	<?php include_once('inc/regular_ticket.html');?>
+</div>
+<div id="tabs-2">
+	<?php include_once('inc/wrkbnchForms.html');?>
+</div>
+</div>
+</body>
+</html>
