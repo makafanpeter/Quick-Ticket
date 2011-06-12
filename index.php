89903ba56e8d14f3a080f9b6bc1123bfc72209ff
thath <thath@pdx.edu>
2011-06-12 11:31:42 -0700

    added facility to make admin menu appear only for those on the admin list

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 
-// import phpCAS lib
+// import phpCAS lib, and mysql connect files
 include_once('CAS.php');
+include_once('bin/db_connect.php');
+include_once('/home/thath/.mysql_connect.php');
 
 //phpCAS::setDebug();
 
@@ -22,10 +24,29 @@ $userArray = explode(',',$tempString);
 
 //set username variable
 $user = phpCAS::getUser();
-
 // Check to see if users are in the Array
 if (!in_array($user, $userArray)){
 	header('Location: https://oit.pdx.edu/qt/notregistered.html');
+}
+
+$admin_user = array();
+$connect = new mysqlConn();
+
+$con = $connect->Connect($host, $db_user, $pass);
+
+$connect->Select($con,$db);
+$query = "SELECT * FROM qt_admin_users";
+
+$result = mysql_query($query);
+
+if(!$result) {
+	return;
+}else{
+	while($row = mysql_fetch_assoc($result)){
+		$admin_user['username'] = $row['username'];
+		$admin_user['f_name']   = $row['f_name'];
+		$admin_user['l_name']   = $row['l_name'];
+	}
 }
 ?>
 <!DOCTYPE XHTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
@@ -59,8 +80,11 @@ $(document).ready(function(){
 	$('#tabs-2 #quickSubject').change(function() {
 		setFormSubject('tabs-2');
 	});
-	$('.user-data button').click(function() {
-		alert($('body div #creator').val());
+	$('#tabs-1 .user-data button').click(function() {
+		createTicket('tabs-1');	
+	});
+	$('#tabs-2 .user-data button').click(function() {
+		createTicket('tabs-2');	
 	});
 });
 	$(function() {
@@ -70,13 +94,16 @@ $(document).ready(function(){
 </script>
 </head>
 <body> 
-<div id="creator" style="display: none"><?php echo phpCAS::getUser();?></div> 
+<div id="creator" style="display: none"><?php echo $user;?></div> 
 <h1>Quick Ticket</h1><img id="psuLogo" src="images/psu_logo.gif"/>
 
 <div id="tabs">
 	<ul>
 		<li><a id="tablink-1" href="#tabs-1">Regular Ticket</a></li>
 		<li><a id="tablink-2" href="#tabs-2">Workbench Ticket</a></li>
+		<?php if (in_array($user,$admin_user)){ 
+			echo '<li><a id="tablink-3" href="#tabs-3">Admin Menu</a></li>';
+		}?>
 	</ul>
 <div id="tabs-1">
 	<?php include_once('inc/regular_ticket.html');?>
@@ -84,6 +111,11 @@ $(document).ready(function(){
 <div id="tabs-2">
 	<?php include_once('inc/wrkbnchForms.html');?>
 </div>
+<div id="tabs-3">
+	<?php
+		print_r($admin_user);
+	?>
+</div>	
 </div>
 </body>
 </html>
